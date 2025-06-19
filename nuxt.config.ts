1e2788ecc6485a8149ab6387f0d50dca62fdb753
양진 <yjineey@gmail.com>
2025-06-20 01:36:47 +0900

    feat: GitHub Pages 배포 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   css: ['~/assets/css/tailwind.css'],
+
+  // https://nuxt.com/docs/guide/deploy/github-pages
+  ssr: false,
+  app: {
+    baseURL: process.env.APP_BASE_URL || '/',
+  },
+
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.PUBLIC_API_URL,
+    },
+  },
 })
